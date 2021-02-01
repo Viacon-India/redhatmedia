a27d592bd7ce5aef26e6d5b79f27a1f21e840a66
sharmita95 <viacon.sharmita@gmail.com>
2021-02-01 15:22:03 +0530

    changes 12

--- wp-content/themes/RHM-Blog-Theme-8/template-parts/content-news-sidebar.php
+++ wp-content/themes/RHM-Blog-Theme-8/template-parts/content-news-sidebar.php
@@ -1,5 +1,10 @@
 <aside class="aside-bar">
 
+	<div class="social-media-section custom-widget">
+		<h5 class="asid-content-title">Subscribe To Our Newsletter</h5>
+		<?php echo do_shortcode('[email-subscribers-form id="1"]'); ?>			
+	</div>
+	
 	<div class="popular-posts custom-widget">
 		<div class="social-media-section">
 			<?php echo do_shortcode('[get_popular_news_posts]'); ?>
