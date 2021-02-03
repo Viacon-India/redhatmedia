692b246dbc8d29f99e79ac2626226ee81fe8e289
dipankarviacon123 <dipankarbarman.viacon@gmail.com>
2021-02-03 16:41:25 +0530

    add brake point

--- wp-content/themes/RHM-Blog-Theme-8/page-template/news-page.php
+++ wp-content/themes/RHM-Blog-Theme-8/page-template/news-page.php
@@ -291,6 +291,20 @@ get_footer(); ?>
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+        }
   });
 
   function getDirection() {
